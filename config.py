5ebd0eba10fcbf51fc6f393fbca419cf96629f73
vrajpatel2004 <vraj931644A>
2025-02-14 11:48:58 +0530

    Remove all files

--- config.py
+++
@@ -1,8 +0,0 @@
-gmail_username = "vrajpatel0218@gmail.com"
-gmail_app_password = "uylm slxy cgqe qwkg"
-imap_server = "imap.gmail.com"
-apikey = "173b1d0a-5048-43d4-b23c-8cb1b413d676"
-secretkey ="q756s0sih5"
-redirect_uri="http://localhost:3000"
-PHONE_NUMBER = 9316443359
-PASSWORD = 310102
--- temp.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, jsonify
-import requests
-from pymongo import MongoClient
-import copy
-
-client = MongoClient("mongodb+srv://test-yt:DYYAQ8YZ2d1XrdTb@test.ced18.mongodb.net/")
-db = client["user_database"]
-collection = db["users"]
-collection.insert_one({"vraj":"patel"})
