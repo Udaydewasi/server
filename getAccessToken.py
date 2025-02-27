a5118f2354bcb72f15623d5bc84b910f7914a652
Udaydewasi <udaydewasi2601@gmail.com>
2025-02-27 11:01:42 +0000

    added chromium for selenium

--- getAccessToken.py
+++ getAccessToken.py
@@ -1,5 +1,6 @@
-from selenium.webdriver.common.by import By
-from seleniumbase import Driver
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import config
 from imap_tools import MailBox, A
 from bs4 import BeautifulSoup
